a41eb400bbb71583153c3fea5397db7e513eb3c8
sleepy-droid <sebastian.sanchez1@utp.ac.pa>
2023-11-28 11:55:59 -0500

    Se agrega funcionalidades en el php

---
+++ modelo_base_de_datos.php
@@ -0,0 +1,30 @@
+<?php
+include "BaseDeDatos.php";
+function insertarDatos()
+{
+    // Verificar si se ha recibido información del formulario
+    if (isset($_POST['submit'])) {
+        // Establecer la variable $connection como global
+        global $connection;
+
+        // Asignar los parámetros recibidos del formulario
+        $username = $_POST['username'];
+        $contrasena = $_POST['contrasena'];
+
+        // Crear la variable $query con el script de inserción
+        $query = "INSERT INTO usuarios(username, contrasena) VALUES ('$username', '$contrasena')";
+
+        // Ejecutar el query y asignar el resultado a $result
+        $result = mysqli_query($connection, $query);
+
+        // Validar si el registro se ha agregado o no
+        if ($result) {
+            echo "Registro realizado con éxito";
+        } else {
+            die('La inserción de los datos ha fallado ' . mysqli_error($connection));
+        }
+
+        // Cerrar la conexión a la base de datos
+        mysqli_close($connection);
+    }
+}
